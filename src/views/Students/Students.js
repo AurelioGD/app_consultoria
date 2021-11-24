7402e584a22c0128c3d0571659faf4ee665d5abe
Aurelio Gonzalez <agg156231@gmail.com>
2021-11-23 22:32:36 -0700

    Se inicio con las tablas

--- src/views/Students/Students.js
+++ src/views/Students/Students.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
+
+const a = [
+    {
+        name:"Hila",
+        username:"hilary"
+    },
+    {
+        name:"Aurelio",
+        username:"AurelioGD"
+    }
+]
 
 const Students = () => {
     return (
         <div>
-            
+            <div className="card">
+                <DataTable value={a} responsiveLayout="scroll">
+                    <Column field="name" header="Name"></Column>
+                    <Column field="username" header="Username"></Column>
+                </DataTable>
+            </div>
         </div>
     )
 }
